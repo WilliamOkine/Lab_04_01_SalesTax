a9fdaf59cd405a87ae0fb64893ade44701041aa9
WilliamOkine <okinewn@mail.uc.edu>
2023-06-05 12:44:10 -0400

    Initial commit

---
+++ src/SalesTax.java
@@ -0,0 +1,9 @@
+public class SalesTax {
+
+    public static void main(String[] args) {
+        double price = 100;
+        double tax = price * 0.05;
+        System.out.println("Purchase Price : $" + price);
+        System.out.println("Sales Tax: $" + tax);
+    }
+}
